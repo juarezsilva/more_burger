3a9179f844bf10f06030823086725ccfd71f653a
Juarez Santos <juarez_santos2912@yahoo.com.br>
2023-07-20 14:39:09 -0300

    atualizado

---
+++ src/components/Banner.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="main-banner">
+        <h1>More Burger</h1>                  
+    </div>      
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner {  
+        background-image: url('/public/img/burger.jpg');
+        background-position: 0px -180px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;                 
+    }
+
+    #main-banner h1 {
+        color: #777777;
+        text-align: center;
+        font-size: 60px;
+        background-color: rgba( 255, 255, 255, 0.100);
+        border: 2px solid rgba( 255, 255, 255, 0.300 );
+        padding: 20px 60px;
+        border-radius: 0px 5px 5px 0px;
+        -webkit-text-stroke-width: 1px;
+        -webkit-text-stroke-color: #444444; 
+    }
+
+    /* MEDIA SCREEN */
+
+    @media screen and (max-width: 1064px) {
+        #main-banner {
+            height: 480px;
+            background-position: 0px -100px ;
+        } 
+        
+        #main-banner h1 {
+        font-size: 50px;        
+        padding: 15px 40px;        
+        } 
+    }
+
+    @media screen and (max-width: 880px) {
+        #main-banner {
+            height: 410px;
+            background-position: 0px -70px ;
+        }         
+    }
+
+    @media screen and (max-width: 768px) {
+        #main-banner {
+            height: 400px;
+            background-position: 0px -35px;
+        } 
+        
+        #main-banner h1 {
+        font-size: 42px;        
+        padding: 15px 40px;        
+        }        
+    }
+
+    @media screen and (max-width: 670px) {
+        #main-banner {
+            height: 350px;
+            background-position: 0px 0px ;
+        }  
+        
+        #main-banner h1 {
+        font-size: 35px;        
+        padding: 15px 40px;        
+        }
+    }
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="nav">
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
+        </router-link>
+        <router-link to="/" id="flash">Home</router-link>
+        <router-link to="/pedidos" id="flash">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: ["logo", "alt"]
+}
+</script>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #flash {
+        background-color: #222;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    #flash:hover {
+        background-color: #333;         
+    }
+
+    #nav a {
+        color: #FCBA03;
+        text-decoration: none;
+        font-size: 18px;
+        margin: 12px;        
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: cyan;
+    }
+
+</style>
